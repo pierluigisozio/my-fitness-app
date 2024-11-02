e498dcf06a4965adced465bef04cf192cdaea349
PSOZIO <pieluigi.sozio@vodafone.com>
2024-11-02 23:02:19 +0100

    come back to sqlite instead of postgreesql

--- auth_project/auth_project/settings.py
+++ auth_project/auth_project/settings.py
@@ -91,12 +91,8 @@ WSGI_APPLICATION = 'auth_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'auth_system',
-        'USER': 'psozio',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
