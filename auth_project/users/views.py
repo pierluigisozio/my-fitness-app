91b774f97433674f666680c6ab6cef535e118c74
PSOZIO <pieluigi.sozio@vodafone.com>
2024-11-02 23:33:29 +0100

    creating api in views

--- auth_project/users/views.py
+++ auth_project/users/views.py
@@ -1,1 +1,51 @@
+from rest_framework import status
+from rest_framework.decorators import api_view
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 
+
+@api_view('POST')
+def register_by_username_and_password(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    if username and password:
+        if User.objects.filter(username=username).exists():  # Non ho ben capito l'oggetto User
+            return Response({'error': 'username invalid'}, status.HTTP_400_BAD_REQUEST)
+
+        user = User.objects.create_user(username=username, password=password)
+        return Response({'message': "user registered successfully"}, status=status.HTTP_201_CREATED)
+
+    return Response({'error': 'Missing fields'}, status.HTTP_400_BAD_REQUEST)
+    # todo: it should be login with email and password
+
+
+@api_view('POST')
+def login_by_username_and_password(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    try:
+        user = User.objects.get(username=username)
+        if user.check_password(password):
+            refresh = RefreshToken.for_user(user)
+            return Response({'access': str(refresh.access_token), 'refresh': str(refresh)}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+    except User.DoesNotExist:
+        return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+@api_view('POST')
+def logout(request):
+    refresh_token = request.data.get('refresh')
+
+    if refresh_token:
+        try:
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({'message': 'Logged out successfully'}, status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+    return Response({'error': 'Missing refresh token'}, status=status.HTTP_400_BAD_REQUEST)
