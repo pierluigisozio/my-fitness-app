636eae93f65920a1667d2fbe8c15291e55312d38
PSOZIO <pieluigi.sozio@vodafone.com>
2024-11-08 17:25:17 +0100

    Login component

--- auth-frontend/src/app/components/login/login.component.ts
+++ auth-frontend/src/app/components/login/login.component.ts
@@ -1,12 +1,62 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NavigationService} from '../../services/navigation.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {UserService} from '../../services/user.service';
+import {CommonModule} from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { IUserPayload } from '../../models/users.model';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  fromRegistration? : boolean;
+  loginForm: FormGroup;
+
+
+  constructor(
+    private navigationService : NavigationService,
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private router: Router
+  ) {
+    this.loginForm = this.formBuilder.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  }
+  ngOnInit(): void {
+    this.fromRegistration = this.route.snapshot.queryParams['fromRegistration'] === 'true';
+  }
+  goToPage(page: string){
+    this.navigationService.redirectTo(page);
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const {username, password} = this.loginForm.value;
+
+      const payload : IUserPayload = {username, password};
+      this.userService.loginUsername(payload).subscribe({
+        next: (response) => {
+          console.log('Login successful', response);
+          localStorage.setItem('access_token', response.access)
+          this.goToPage('profile');
+        },
+        error: (err) =>{
+          console.error('Error performing login', err);
+        }
+      })
+    }
+  }
+
 
 }
