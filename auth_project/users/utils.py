c254edf5fa14f6ecfd19f171058f4ec22a4ce829
PSOZIO <pieluigi.sozio@vodafone.com>
2024-11-08 18:28:43 +0100

    changed regex for testing

--- auth_project/users/utils.py
+++ auth_project/users/utils.py
@@ -2,7 +2,7 @@ import re
 
 
 def is_valid_password(password):
-    pattern = r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{8,}$'
+    pattern = '^.{6,}$'
 
     if re.match(pattern, password):
         return True
