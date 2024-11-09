8ae331c6dbd55e6fef8b4164098bb841e5cd9246
PSOZIO <pieluigi.sozio@vodafone.com>
2024-11-09 12:38:48 +0100

    Styling components

--- auth-frontend/src/app/components/dashboard/dashboard.component.ts
+++ auth-frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {NavigationService} from '../../services/navigation.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  constructor(private navigationService : NavigationService){}
+
+  goToPage(page: string){
+    this.navigationService.redirectTo(page);
+  }
 
 }
